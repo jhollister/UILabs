c61d8004f81cc539da32bce002330df3d71cd1cf
jhollister <jhollisterjr@gmail.com>
2014-06-15 19:54:10 -0700

    Removed log messages for test cases

--- app/src/main/java/course/labs/todomanager/ToDoManagerActivity.java
+++ app/src/main/java/course/labs/todomanager/ToDoManagerActivity.java
@@ -82,15 +82,13 @@ public class ToDoManagerActivity extends ListActivity {
 
         super.onActivityResult(requestCode, resultCode, data);
 
-		log("Entered onActivityResult()");
-        log("Request code: " + requestCode + " Result Code: " + resultCode);
+		//log("Entered onActivityResult()");
 
 		// Check result code and request code.
 		// If user submitted a new ToDoItem
 		// Create a new ToDoItem from the data Intent
 		// and then add it to the adapter
         if ((requestCode == ADD_TODO_ITEM_REQUEST) && (resultCode == RESULT_OK)) {
-            log("Request code OK");
             ToDoItem item = new ToDoItem(data);
             mAdapter.add(item);
         }
